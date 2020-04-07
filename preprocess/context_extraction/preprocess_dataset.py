4e18f61f15ad05de8f42dce928e316e34798f305
Griffin Adams <griff4692@gmail.com>
2020-04-07 17:08:44 -0400

    Update preprocessing.

--- preprocess/context_extraction/preprocess_dataset.py
+++ preprocess/context_extraction/preprocess_dataset.py
@@ -1,6 +1,7 @@
 import json
 import os
 import re
+import string
 import sys
 
 import numpy as np
@@ -13,7 +14,7 @@ from compute_sections import HEADER_SEARCH_REGEX
 from canonical_sections import get_canonical_section
 
 
-def get_header_from_full_context(mimic_df, context, lf_match, doc_id, category):
+def get_header_from_full_context(mimic_df, context, lf_match, doc_id, category, sub_header=False):
     """
     :param mimic_df: NOTEEVENTS.csv dataframe
     :param context: local context surrounding expansion
@@ -29,24 +30,40 @@ def get_header_from_full_context(mimic_df, context, lf_match, doc_id, category):
     try:
         pre_idx = full_context.index(context_repl)
     except:
-        pre_idx = full_context.index(context.split('TARGETWORD')[0].strip()[:-2])
+        try:
+            pre_idx = full_context.index(context.split('TARGETWORD')[0].strip()[:-3])
+        except:
+            print('here')
+            if sub_header:
+                return ''
+            else:
+                return create_section_token('{}->START'.format(category))
     relevant_context = full_context[:pre_idx]
     sectioned_text = list(filter(
         lambda x: len(x.strip()) > 0, re.split(HEADER_SEARCH_REGEX, relevant_context, flags=re.M)))
     is_header_arr = list(map(lambda x: re.match(HEADER_SEARCH_REGEX, x, re.M) is not None, sectioned_text))
     headers = []
+    sub_headers = []
     for tok_idx, is_header in enumerate(is_header_arr):
         tok = sectioned_text[tok_idx]
         if is_header and is_actual_header(tok):
             header_stripped = tok.strip().strip(':').upper()
             canonical_section = get_canonical_section(header_stripped, category)
             is_canonical = '->' in canonical_section
+            header = create_section_token(canonical_section, is_canonical)
             if is_canonical:
-                header = create_section_token(canonical_section, is_canonical)
                 headers.append(header)
-    if len(headers) == 0:
-        return create_section_token('{}->START'.format(category))
-    return headers[-1]
+            else:
+                sub_headers.append(header)
+
+    if sub_header:
+        if len(sub_headers) == 0:
+            return ''
+        return sub_headers[-1]
+    else:
+        if len(headers) == 0:
+            return create_section_token('{}->START'.format(category))
+        return headers[-1]
 
 
 def preprocess_mimic_rs(window=10):
@@ -64,12 +81,14 @@ def preprocess_mimic_rs(window=10):
     if os.path.exists(remote_path):
         mimic_df = pd.read_csv(remote_path)
     else:
-        mimic_df = pd.read_csv(os.path.join(home_dir, 'preprocess', 'data', 'mimic', 'NOTEEVENTS.csv'))
+        mimic_df = pd.read_csv(os.path.join(home_dir, 'preprocess/data/mimic/NOTEEVENTS.csv'))
     df = pd.read_csv('data/mimic_rs_dataset.csv')
     N = df.shape[0]
     tokenized_contexts = []
-    sections = []
+    metadata = []
+    sub_sections = []
     trimmed_contexts = []
+    trimmed_sentences = []
     lf_tokenized_out = set()
 
     print('Tokenizing and extracting context windows...')
@@ -78,21 +97,28 @@ def preprocess_mimic_rs(window=10):
         row = row.to_dict()
         context = row['context']
         category = row['category']
-        tokens = preprocess_mimic((category, context)).split(' ')[1:]
+        tokens_orig = preprocess_mimic((category, context)).split(' ')[1:]
+        tokens = []
+        for token in tokens_orig:
+            for t in re.split(r'(targetword)', token):
+                if len(t) > 0:
+                    tokens.append(t.strip(string.punctuation))
         is_header = list(map(lambda x: 'header=' in x, tokens))
         header_locs = np.where(np.array(is_header))[0]
+        sep_locs = np.where(np.array(tokens) == '<sep>')[0]
         sf_idx = np.where(np.array(tokens) == 'targetword')[0]
 
         if not len(sf_idx) == 1:
             is_valid.append(False)
             tokenized_contexts.append(None)
-            sections.append(None)
+            metadata.append(None)
+            sub_sections.append(None)
             trimmed_contexts.append(None)
-            print('LF={} was tokenized out of context'.format(row['lf']))
+            trimmed_sentences.append(None)
+            # print('LF={} was tokenized out of context'.format(row['lf']))
             lf_tokenized_out.add(row['lf'])
         else:
             sf_idx = sf_idx[0]
-
             if len(header_locs) > 0:
                 header_rel = header_locs - sf_idx
                 header_rel_pos_mask = np.where(header_rel > 0)[0]
@@ -115,8 +141,31 @@ def preprocess_mimic_rs(window=10):
                 left_header_window = None
                 right_header_window = None
 
+            if len(sep_locs) > 0:
+                sep_rel = sep_locs - sf_idx
+                sep_rel_pos_mask = np.where(sep_rel > 0)[0]
+                sep_rel_neg_mask = np.where(sep_rel < 0)[0]
+
+                sep_rel_left = sep_rel.copy()
+                sep_rel_left[sep_rel_pos_mask] = -100
+
+                sep_rel_right = sep_rel.copy()
+                sep_rel_right[sep_rel_neg_mask] = 100
+
+                left_sep_window = sep_locs[sep_rel_left.argmax()]
+                right_sep_window = sep_locs[sep_rel_right.argmin()]
+
+                if left_sep_window > sf_idx:
+                    left_sep_window = None
+                if right_sep_window < sf_idx:
+                    right_sep_window = None
+            else:
+                left_sep_window = None
+                right_sep_window = None
+
             left_header = tokens[left_header_window] if left_header_window is not None else None
             left_header_boundary = 0 if left_header is None else left_header_window + 1
+            left_sep_boundary = 0 if left_sep_window is None else left_sep_window + 1
 
             if left_header is None:
                 left_header = get_header_from_full_context(mimic_df, context, row['lf_match'], row['doc_id'], row['category'])
@@ -124,16 +173,29 @@ def preprocess_mimic_rs(window=10):
             if len(left_header) > 100:
                 left_header = get_header_from_full_context(mimic_df, context, row['lf_match'], row['doc_id'], row['category'])
             right_header_boundary = len(tokens) if right_header_window is None else right_header_window
+            right_sep_boundary = len(tokens) if right_sep_window is None else right_sep_window
 
             left_window = max(sf_idx - window, 0, left_header_boundary)
+            left_window_sentence = max(left_window, left_sep_boundary)
             right_window = min(sf_idx + window + 1, len(tokens), right_header_boundary)
-            window_tokens = tokens[left_window:sf_idx] + tokens[sf_idx + 1:right_window]
+            right_window_sentence = min(right_window, right_sep_boundary)
+            window_tokens = tokens[left_window:sf_idx] + [row['sf']] + tokens[sf_idx + 1:right_window]
+            window_tokens_sentence = tokens[left_window_sentence:sf_idx] + [row['sf']] + tokens[sf_idx + 1:right_window_sentence]
+
+            has_sub = any(['sub=' in t for t in tokens[left_window:sf_idx]])
+            sub_header = None
+            if not has_sub:
+                sub_header = get_header_from_full_context(
+                    mimic_df, context, row['lf_match'], row['doc_id'], row['category'], sub_header=True)
 
             if len(window_tokens) == 0:
                 trimmed_contexts.append('<pad>')
+                trimmed_sentences.append('<pad>')
             else:
                 trimmed_contexts.append(' '.join(window_tokens))
-            sections.append(left_header)
+                trimmed_sentences.append(' '.join(window_tokens_sentence))
+            metadata.append(left_header)
+            sub_sections.append(sub_header)
             tokenized_contexts.append(' '.join(tokens))
             is_valid.append(True)
         if (row_idx + 1) % 1000 == 0:
@@ -144,7 +206,8 @@ def preprocess_mimic_rs(window=10):
     df.rename(columns={'lf': 'target_lf'}, inplace=True)
     df['trimmed_tokens'] = trimmed_contexts
     df['tokenized_context'] = tokenized_contexts
-    df['metadata'] = sections
+    df['tokenized_context_sentence'] = trimmed_sentences
+    df['metadata'] = metadata
     df['is_valid'] = is_valid
 
     df = df[df['is_valid']]
